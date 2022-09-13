53027ef5b9848de279fd1b4798eb81e9b83a88f4
NotCuthulu <112963197+NotCuthulu@users.noreply.github.com>
2022-09-13 08:13:59 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    TrueOrDare = Math.randomBoolean()
+    if (true) {
+        basic.showString("truth")
+    }
+    if (false) {
+        basic.showString("Dare")
+    }
+})
+let TrueOrDare = false
+TrueOrDare = 0
