da2b26b95b488370af570e62752d8272666f6a38
NotCuthulu <112963197+NotCuthulu@users.noreply.github.com>
2022-09-13 08:29:23 -0600

    Finished it

--- main.ts
+++ main.ts
@@ -1,11 +1,51 @@
 input.onButtonPressed(Button.A, function () {
-    TrueOrDare = Math.randomBoolean()
-    if (true) {
-        basic.showString("truth")
+    TrueOrDare = randint(0, 1)
+    if (TrueOrDare == 0) {
+        basic.showString("Truth")
     }
-    if (false) {
+    if (TrueOrDare == 1) {
         basic.showString("Dare")
     }
 })
-let TrueOrDare = false
+input.onButtonPressed(Button.B, function () {
+    Arrow_Direction = randint(0, 3)
+    if (Arrow_Direction == 0) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
+    if (Arrow_Direction == 1) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    }
+    if (Arrow_Direction == 2) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    }
+    if (Arrow_Direction == 3) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+})
+let Arrow_Direction = 0
+let TrueOrDare = 0
 TrueOrDare = 0
